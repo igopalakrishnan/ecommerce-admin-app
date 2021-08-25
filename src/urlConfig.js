b516cd1ae732ea19414c88aaa45b02321c0a8035
igopalakrishnan <kingkrishna809@gmail.com>
2021-08-25 16:50:49 +0530

    change urlconfig

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,8 @@
-export const api = 'http://localhost:2000/api';
+const baseUrl = process.env.API || "https://flipkart-rest-server.herokuapp.com";
+//const baseUrl = "http://localhost:2000";
+
+export const api = `${baseUrl}/api`;
+
 export const generatePublicUrl = (fileName) => {
-    return `http://localhost:2000/public/${fileName}`;
-}
+  return `${baseUrl}/public/${fileName}`;
+};
