d32f70806a2f80e0e6948472132a784ec6ec75ca
igopalakrishnan <kingkrishna809@gmail.com>
2021-08-26 12:26:00 +0530

    update url config

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -4,3 +4,12 @@ export const baseUrl = "https://flipkart-back-end-server.herokuapp.com/"
 export const generatePublicUrl = (fileName) => {
     return `${baseUrl}/public/${fileName}`;
 }
+
+
+//export const baseUrl = "https://flipkart-back-end-server.herokuapp.com/"
+
+export const api = 'http://localhost:2000/api';
+export const generatePublicUrl = (fileName) => {
+    return `http://localhost:2000/public/${fileName}`;
+}
+
